ecbf2a9d743bec7bf7bf245c45efeae24a095079
Patricio Lopez <pelopez2@uc.cl>
2014-12-20 18:19:10 -0300

    grunt build won't be on server

--- config/deploy.rb
+++ config/deploy.rb
@@ -87,7 +87,7 @@ task :deploy => :environment do
     to :launch do
       queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
       queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
-      queue "cd #{deploy_to}/#{current_path}/ng-app && grunt build --force && cd ../../.." # Grunt build
+      # queue "cd #{deploy_to}/#{current_path}/ng-app && grunt build --force && cd ../../.." # Grunt build
     end
   end
 end
