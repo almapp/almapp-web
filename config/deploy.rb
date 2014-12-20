d5fc500cc2268165773e788e03c48256bd694e69
Patricio Lopez <pelopez2@uc.cl>
2014-12-20 17:41:36 -0300

    added mina deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
 # require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
-# require 'mina/rvm'    # for rvm support. (http://rvm.io)
+require 'mina/rvm'    # for rvm support. (http://rvm.io)
 
 # Basic settings:
 #   domain       - The hostname to SSH to.
@@ -10,26 +10,44 @@ require 'mina/git'
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
 
-set :domain, 'foobar.com'
-set :deploy_to, '/var/www/foobar.com'
-set :repository, 'git://...'
-set :branch, 'master'
+set :domain,      'almapp.me'
+set :deploy_to,   '/home/deploy/almapp'
+set :repository,  'git@github.com:almapp/almapp-web.git'
+set :branch,      'master'
+set :user,        'deploy'
+set :port,        '1813'
 
 # For system-wide RVM install.
 #   set :rvm_path, '/usr/local/rvm/bin/rvm'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'log']
+set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'log']
 
 # Optional settings:
 #   set :user, 'foobar'    # Username in the server to SSH to.
 #   set :port, '30000'     # SSH port number.
 #   set :forward_agent, true     # SSH forward_agent.
 
+task :logs do
+  queue 'echo "Contents of the log file are as follows:"'
+  queue "tail -f /var/log/apache.log"
+end
+
 # This task is the environment that is loaded for most commands, such as
 # `mina deploy` or `mina rake`.
 task :environment do
+
+  ##########
+  # From http://winstonyw.com/2014/10/24/setting_up_ruby_on_rails_on_digital_ocean/
+  #####
+
+  queue 'source ~/.bash_profile'
+  # For those using RVM, use this to load an RVM version@gemset.
+  invoke :'rvm:use[ruby-2.1.4]'
+
+  ##########
+
   # If you're using rbenv, use this to load the rbenv environment.
   # Be sure to commit your .rbenv-version to your repository.
   # invoke :'rbenv:load'
@@ -50,6 +68,8 @@ task :setup => :environment do
 
   queue! %[touch "#{deploy_to}/#{shared_path}/config/database.yml"]
   queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/database.yml'."]
+  queue! %[touch "#{deploy_to}/#{shared_path}/config/secrets.yml"]
+  queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/secrets.yml'."]
 end
 
 desc "Deploys the current version to the server."
@@ -61,15 +81,25 @@ task :deploy => :environment do
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
-    invoke :'rails:assets_precompile'
+    # invoke :'rails:assets_precompile' It's a Rails-API app
     invoke :'deploy:cleanup'
 
     to :launch do
       queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
       queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
+      queue "cd #{deploy_to}/#{current_path}/ng-app && grunt build --force && cd ../../.." # Grunt build
     end
   end
 end
+
+desc "Seed data to the database"
+task :seed => :environment do
+  queue "cd #{deploy_to}/#{current_path}/"
+  queue "bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+  queue  %[echo "-----> Rake Seeding Completed."]
+end
+
+set :term_mode, nil
 
 # For help in making your deploy script, see the Mina documentation:
 #
