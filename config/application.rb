5c9b090a4425291f138f24c4610368797285a303
Patricio López <lopezjuripatricio@gmail.com>
2015-01-11 08:02:17 -0300

    added gzip compression

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module AlmappWeb
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.middleware.use Rack::Deflater
+
     config.autoload_paths += %W(#{config.root}/lib)
     config.autoload_paths += Dir["#{config.root}/lib/loaders/**/"]
   end
