6ca5861756462668db279bdd0e8e2c223bd13467
Patricio López <lopezjuripatricio@gmail.com>
2015-01-16 00:08:30 -0300

    better 'likable' mixin

--- app/models/concerns/likeable.rb
+++ app/models/concerns/likeable.rb
@@ -4,6 +4,21 @@ module Likeable
   included do
     has_many :likes, -> { positive }, as: :likeable
     has_many :dislikes, -> { negative }, as: :likeable, class_name: 'Like'
+
+    has_many :liked_by_users, through: :likes, source: :user
+    has_many :disliked_by_users, through: :dislikes, source: :user
+  end
+
+  def set_like_by (user)
+    like = Like.create!(user:user, valuation:1, likeable:self)
+    likes << like
+    self.save!
+  end
+
+  def set_dislike_by (user)
+    like = Like.create!(user:user, valuation:-1, likeable:self)
+    likes << like
+    self.save!
   end
 
   def like_count
