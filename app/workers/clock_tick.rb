59ab112cf40029672b4cf6dd4560fac3838be00b
Patricio López <lopezjuripatricio@gmail.com>
2015-02-14 11:42:48 -0300

    removed clock tick worker (timezone worked)

--- app/workers/clock_tick.rb
+++ app/workers/clock_tick.rb
@@ -3,8 +3,5 @@ class ClockTick
 
   def perform
     puts 'tick'
-    c = Campus.first
-    c.name = "#{c.name}1"
-    c.save!
   end
 end
--- lib/clock.rb
+++ lib/clock.rb
@@ -6,4 +6,4 @@ include Clockwork
 
 every(1.day, 'Refresh courses', at: '04:30', tz: 'America/Santiago') { Sidekiq::Client.enqueue(Scrapper, current_year, current_period) }
 
-every(1.minute, 'Test clock') { Sidekiq::Client.enqueue(ClockTick) }
+# every(1.minute, 'Test clock') { Sidekiq::Client.enqueue(ClockTick) }
