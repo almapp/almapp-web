a4e92dffa19cc0b8521849531f3d0a19595bbed0
Patricio López <lopezjuripatricio@gmail.com>
2015-02-14 11:39:47 -0300

    will try 'America/Santiago' as time zone

--- lib/clock.rb
+++ lib/clock.rb
@@ -4,6 +4,6 @@ require 'clockwork'
 
 include Clockwork
 
-every(1.day, 'Refresh courses', at: '04:30', tz: 'UTC') { Sidekiq::Client.enqueue(Scrapper, current_year, current_period) }
+every(1.day, 'Refresh courses', at: '04:30', tz: 'America/Santiago') { Sidekiq::Client.enqueue(Scrapper, current_year, current_period) }
 
 every(1.minute, 'Test clock') { Sidekiq::Client.enqueue(ClockTick) }
